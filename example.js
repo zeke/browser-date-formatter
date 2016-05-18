dd2c518d98bf5e2d62e899dfabeaf47580a4ab09
Zeke Sikelianos <zeke@sikelianos.com>
2016-05-17 22:57:02 -0700

    one point oh

---
+++ example.js
@@ -0,0 +1,27 @@
+require
+
+var domready = require('domready')
+var strftime = require('prettydate').strftime
+var relative = require('relative-date')
+
+module.exports = function () {
+  domready(formatDates)
+  setInterval(formatDates, 5 * 1000)
+}
+
+var formatDates = function () {
+  var dates = document.querySelectorAll('[data-date]')
+
+  Array.prototype.forEach.call(dates, function(el) {
+    var date = new Date(el.dataset.date)
+
+    if (!date.getFullYear()) {
+      return console.error('Invalid date', el.dataset.date)
+    }
+
+    var format = el.dataset.format || '%Y-%m-%d'
+    var result = format === 'relative' ? relative(date) : strftime(date, format)
+
+    el.textContent = result
+  })
+}
