77944078eb53b92cef49d30279da21a291597c81
Zeke Sikelianos <zeke@sikelianos.com>
2016-05-28 20:43:59 -0700

    simplify tests

--- test/index.js
+++ test/index.js
@@ -9,8 +9,7 @@ tape('formatter', function (t) {
   document.body.innerHTML = html
   formatter()
   t.ok($('#default').text().match(/ ago$/), 'uses a relative date string by default')
-  t.equal($('#custom-strftime-1').text(), '2016-04-07', 'defaults to Y-M-D format')
-  t.equal($('#custom-strftime-2').text(), '2016', 'supports custom strftime formats')
+  t.equal($('#strftime').text(), '2016-04-07', 'supports custom strftime formats')
   t.end()
   window.close()
 })
