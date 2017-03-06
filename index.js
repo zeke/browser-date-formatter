22f22c116a9e60527e2fb6a2b0903d7c7ccb7daa
Zeke Sikelianos <zeke@sikelianos.com>
2017-03-06 12:29:27 -0800

    support YYYY-MM-DD input format

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ var formatDates = function () {
   var dates = document.querySelectorAll('[data-date]')
 
   Array.prototype.forEach.call(dates, function(el) {
-    var date = new Date(el.dataset.date)
+    // Allow `YYYY-MM-DD` strings as input
+    if (el.dataset.date.match(/^\d{4}-\d{2}-\d{2}$/)) {
+      var parts = el.dataset.date.split('-')
+      var date = new Date(parts[0], parts[1] - 1, parts[2])
+    } else {
+      var date = new Date(el.dataset.date)
+    }
 
     if (!date.getFullYear()) {
       return console.error('Invalid date', el.dataset.date)
